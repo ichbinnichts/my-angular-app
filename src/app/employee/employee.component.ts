d974d31d92cedf4161a28980ff93058bbc6f9d11
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-27 14:38:17 -0300

    feat: add employee component

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'employee-component',
+    templateUrl: './employee.component.html',
+    styleUrls: ['./employee.component.css']
+})
+
+
+export class EmployeeComponent{
+
+}
