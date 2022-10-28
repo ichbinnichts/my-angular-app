abe0038c60f5d1b517efa0cff2d68910c1ecf06f
Nathan Víctor de Faria <nathan.faria@inatel.br>
2022-10-28 08:39:29 -0300

    feat: add ng model and button

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -10,7 +10,11 @@ import { Component } from "@angular/core";
 export class EmployeeComponent{
     title: string = 'My app';
     employeeName: string = "";
+    createdEmployee: string = "";
 
+    createEmployee(){
+        this.createdEmployee = "Employee with name "+this.employeeName+ " was created";
+    }
     updateEmployeeName(event: Event){
         this.employeeName = (<HTMLInputElement>event.target).value;
     }
